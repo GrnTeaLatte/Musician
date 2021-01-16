1c0ea4b8dc205d9c154e8eda737f88e032fe3be0
bundelabun <audreyfu14@gmail.com>
2021-01-15 16:16:32 -0800

    WIP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Landing from './components/Landing';
+import Library from './components/Library';
+import Album from './components/Album';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+              <header>
+                <nav className="navbar">
+                    <img className="logo" src="images/headset_logo.png" alt="bloc jams logo" />
+                    <Link to='/'>Landing</Link>
+                    <Link to='/library'>Library</Link>
+                </nav>
+                <h1>Musician</h1>
+              </header>
+              <main>
+                <Route exact path="/" component={Landing} />
+                <Route path="/library" component={Library} />
+                <Route path="/album/:slug" component={Album} />
+              </main>
+            </div>
+          );
+        }
+    }
 
 export default App;
---
+++ src/assets/scripts/album.js
@@ -0,0 +1,65 @@
+ var createSongRow = function(songNumber, songName, songLength) {
+     var template =
+        '<tr class="album-view-song-item">'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+      + '  <td class="song-item-title">' + songName + '</td>'
+      + '  <td class="song-item-duration">' + songLength + '</td>'
+      + '</tr>'
+      ;
+
+     var $row = $(template);
+
+     var clickHandler = function() {
+         // clickHandler logic
+     };
+
+     var onHover = function(event) {
+         // Placeholder for function logic
+     };
+     var offHover = function(event) {
+         // Placeholder for function logic
+     };
+
+     $row.find('.song-item-number').click(clickHandler);
+     // #2
+     $row.hover(onHover, offHover);
+     // #3
+     return $row;
+ };
+
+  var setCurrentAlbum = function(album) {
+     // #1
+     var $albumTitle = $('.album-view-title');
+     var $albumArtist = $('.album-view-artist');
+     var $albumReleaseInfo = $('.album-view-release-info');
+     var $albumImage = $('.album-cover-art');
+     var $albumSongList = $('.album-view-song-list');
+
+
+     // #2
+     $albumTitle.text(album.title);
+     $albumArtist.text(album.artist);
+     $albumReleaseInfo.text(album.year + ' ' + album.label);
+     $albumImage.attr('src', album.albumArtUrl);
+
+     // #3
+     $albumSongList.empty();
+
+     // #4
+     for (var i = 0; i < album.songs.length; i++) {
+         var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+         $albumSongList.append($newRow);
+     }
+ };
+
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+ var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+ var currentlyPlayingSong = null;
+ var currentAlbum = null;
+ var currentlyPlayingSongNumber = null;
+ var currentSongFromAlbum = null;
+
+ $(document).ready(function() {
+      setCurrentAlbum(albumPicasso);
+ });
